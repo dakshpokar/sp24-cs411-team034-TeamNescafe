a4c873caa35a9091d486808e7f5f1425e0275237
Mehul Oswal <mehuloswal21@gmail.com>
2024-04-19 02:58:13 -0500

    Added Pagination

--- frontend/src/app/(customer)/nav-item.js
+++ frontend/src/app/(customer)/nav-item.js
@@ -4,16 +4,18 @@ import clsx from 'clsx';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
-export function NavItem({ href, children }) {
+export function NavItem({ where, children }) {
 	const pathname = usePathname();
 
 	return (
 		<Link
-			href={href}
+			href={where}
+			prefetch={true}
 			className={clsx(
 				'flex items-center gap-3 rounded-lg  px-3 py-2 text-gray-900  transition-all hover:text-gray-900  dark:text-gray-50 dark:hover:text-gray-50',
 				{
-					'bg-gray-100 dark:bg-gray-800': pathname === href,
+					'bg-gray-100 dark:bg-gray-800':
+						pathname === where,
 				}
 			)}
 		>
