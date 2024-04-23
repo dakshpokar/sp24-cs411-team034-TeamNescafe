40c0bee963bf2608628007dd01913a1404cf613f
Daksh Pokar <dakshp2@illinois.edu>
2024-04-23 02:39:24 -0500

    Changed URL

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	env: {
-		BASEURL: 'http://127.0.0.1:5001/',
-	},
-	images: {
-		domains: ['images.pexels.com'],
-	},
+  env: {
+    BASEURL: "https://suitemate.ue.r.appspot.com/",
+  },
+  images: {
+    domains: ["images.pexels.com"],
+  },
 };
 
 export default nextConfig;
