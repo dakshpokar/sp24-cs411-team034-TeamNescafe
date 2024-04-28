65e0d33467a19eb89c42b8606f85b853c45d6433
Mehul Oswal <mehuloswal21@gmail.com>
2024-04-28 18:04:08 -0500

    Refactor analytics service and add advanced properties filter endpoint

--- backend/services/customer.py
+++ backend/services/customer.py
@@ -269,7 +269,7 @@ def check_application_status():
         token = request.headers['Authorization']
         user_id = get_user_id(connection, token)
         unit_id = request.args['unit_id']
-
+       
         conn = connect_to_database()
         if conn:
             try:
@@ -352,5 +352,36 @@ def add_review():
                 conn.close()
         else:
             return jsonify({'error': 'Failed to establish database connection.'}), 500
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+@customer_service.route('/advanced_properties_filter', methods=['GET'])
+def advanced_properties_filter():
+    try:
+        token = request.headers['Authorization']
+        user_id = get_user_id(connection, token)
+        if check_agent_role(connection, user_id):
+            return jsonify({'error': "User is an Agent"}), 403
+        data = request.args
+        flag = sanitize_input(data.get('flag'))
+        pricemin = sanitize_input(data.get('pricemin'))
+        pricemax = sanitize_input(data.get('pricemax'))
+        areamin = sanitize_input(data.get('areamin', ))
+        areamax = sanitize_input(data.get('areamax'))
+        conn = connect_to_database()
+        if conn:
+            cursor = conn.cursor()
+            if(cursor):
+                try:
+                    cursor.callproc('complex_stored_procedure_for_filtering', [flag, areamin, areamax, pricemin, pricemax])
+                    results = []
+                    for result in cursor.stored_results():
+                        results.append(result.fetchall())
+                    print(results)
+                    return jsonify(results)
+                finally:
+                    conn.close()
+            else:
+                return jsonify({'error': 'Failed to establish database connection.'}), 500
     except Exception as e:
         return jsonify({'error': str(e)}), 500
