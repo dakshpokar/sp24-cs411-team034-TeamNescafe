649e6ec1b67e84041d9c37174bc62edb4635e32d
Mehul Oswal <mehuloswal21@gmail.com>
2024-04-30 17:43:13 -0500

    Button Disabled for Unavailable Units

--- frontend/src/app/(customer)/unit_details/[id]/page.js
+++ frontend/src/app/(customer)/unit_details/[id]/page.js
@@ -122,7 +122,11 @@ const UnitDetails = () => {
 
 					<button
 						onClick={handleSubmitApplication}
-						className='bg-orange-500 text-white rounded-lg px-4 py-2 mt-6 hover:bg-orange-600'
+						className={`bg-orange-500 text-white rounded-lg px-4 py-2 mt-6 hover:bg-orange-600 ${
+							!availability &&
+							'cursor-not-allowed bg-orange-200 hover:bg-orange-200'
+						}`}
+						disabled={!availability}
 					>
 						Submit Application
 					</button>
